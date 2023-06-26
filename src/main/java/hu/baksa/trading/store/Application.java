096661c4319ec3549031ac4d96ce6af35d498db8
BaksaZoo <baksazoo@hotmail.com>
2023-06-26 20:12:08 +0200

    feat: change app name to trading store

--- src/main/java/hu/baksa/trading/store/Application.java
+++ src/main/java/hu/baksa/trading/store/Application.java
@@ -1,4 +1,4 @@
-package hu.baksa.trading.auth;
+package hu.baksa.trading.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
